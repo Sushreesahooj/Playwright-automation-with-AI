e7b48c8daa369571bc9a2c47b17c5437f5f442ba
Sushreesahooj <you@example.com>
2026-07-20 03:09:59 +0530

    Add helloworld.js and clean up old test files

--- tests/hello.spec.js
+++
@@ -1,7 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('hello world page loads', async ({ page }) => {
-  await page.goto('https://example.com/');
-  await expect(page).toHaveTitle(/Example Domain/);
-  await expect(page.locator('h1')).toHaveText('Example Domain');
-});
